6cad59dae6408ad7388f87f52d2b2b3708138167
Sandeep Yadav <67813871+SandeepYadav-05@users.noreply.github.com>
2020-08-02 15:49:38 +0530

    Add files via upload

---
+++ Data_Collection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  2 13:49:44 2020
+
+@author: foxbee
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "C:/Users/foxbee/Documents/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 10, False, path, 5)
+
+df.to_csv('glassdoor_jobs.csv', index = False)
