804bb89ba3f0dbfea0b84f446762335190d8bf62
Sandeep Yadav <67813871+SandeepYadav-05@users.noreply.github.com>
2020-08-09 09:24:56 +0530

    Update Data_Collection.py

--- Data_Collection.py
+++ Data_Collection.py
@@ -9,6 +9,6 @@ import glassdoor_scraper as gs
 import pandas as pd
 path = "C:/Users/foxbee/Documents/ds_salary_proj/chromedriver"
 
-df = gs.get_jobs('data scientist', 10, False, path, 5)
+df = gs.get_jobs('data scientist', 1000, False, path, 5)
 
 df.to_csv('glassdoor_jobs.csv', index = False)
